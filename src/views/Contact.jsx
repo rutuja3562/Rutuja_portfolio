6375602d852aa5a30d82de24d817fb307f5ce230
rutuja3562 <rspatil3562@gmail.com>
2022-04-29 12:48:57 +0530

    contact details change

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext ,useState} from "react";
+import React, { useContext, useState } from "react";
 import { contactLinks } from "../constants";
 import { ThemeContext } from "../themeProvider";
 // import { toast } from "react-toastify";
@@ -8,12 +8,8 @@ const Contact = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
-
-    // const { name, email, message } = data;
   return (
-    <div
-      // style={{ border: "3px solid pink" }}
-      id="contact"
+    <div id="contact"
       className={
         darkMode
           ? "bg-gray-100 pt-24 md:h-screen"
@@ -35,61 +31,122 @@ const Contact = () => {
           <h2 className="text-5xl font-bold px-4 md:px-0 text-center z-0">
             Contact
           </h2>
-          <div style={{ textAlign: "center" }}>
+          <div tyle={{ textAlign: "center" }}>
             <h4 className="mt-12 text-3xl font-semibold text-blue-500">
               Get In Touch
             </h4>
             <p className="text-gray-500 text-xl"></p>
           </div>
+
           <div
             // style={{ border: "3px solid teal" }}
             className="flex justify-between items-center md:items-stretch  flex-col md:flex-row pb-24"
           >
+            <div className="w-full md:pr-8">
+              <form
+              //  onSubmit={onSubmitHandler}
+              >
+                <div class="my-6">
+                  <label
+                    for="name"
+                    class={
+                      darkMode
+                        ? "block mb-2 text-lg font-medium text-gray-900"
+                        : "block mb-2 text-lg font-medium text-white"
+                    }
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your name"
+                    // onChange={onChangeHandler}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    for="email"
+                    class={
+                      darkMode
+                        ? "block mb-2 text-lg font-medium text-gray-900"
+                        : "block mb-2 text-lg font-medium text-white"
+                    }
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your email"
+                    // onChange={onChangeHandler}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    for="message"
+                    class={
+                      darkMode
+                        ? "block mb-2 text-lg font-medium text-gray-900"
+                        : "block mb-2 text-lg font-medium text-white"
+                    }
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 h-28 w-full text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your message"
+                    // onChange={onChangeHandler}
+                    required
+                  />
+                </div>
+                <div className="flex justify-between ">
+                  <div className="underline">
+                    <a href="mailto:rspatil3562@gmail.com">
+                      Send me email directly
+                    </a>
+                  </div>
+                  <button className="bg-indigo-500 text-white px-4 py-2 w-40 rounded-md hover:bg-indigo-400">
+                    <a href="mailto:rspatil3562@gmail.com">Submit</a>
+                  </button>
+                </div>
+              </form>
+            </div>
+
             <div
-              className="w-full flex flex-col md:items-end  mt-12 md:mt-6"
+              className="w-full flex flex-col md:items-end   md:mt-12"
               style={{
                 textAlign: "center",
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
               }}
             >
-              <div>
-                <h3 className="text-2xl font-bold">Phone</h3>
-                <a className="mb-5 mt-4 font-semibold text-white-700 block uppercase">
-                  +91 9075613562
-                </a>
-              </div>
-              <div>
-                <h3 className="text-2xl font-bold mb-3"> Email</h3>
+              <h3 className="text-2xl font-bold">Phone</h3>
+              <a className="mb-5 mt-4 font-semibold text-white-700 block uppercase">
+                +91 9075613562
+              </a>
 
-                <a href="mailto:rspatil3562@gmail.com">rspatil3562@gmail.com</a>
-                <br />
-              </div>
+              <h3 className="text-2xl font-bold mb-3"> Email</h3>
+
+              <a href="mailto:rspatil3562@gmail.com">rspatil3562@gmail.com</a>
               <br />
-              <div>
-                <h3 className="text-2xl  font-bold">Social</h3>
+              <h3 className="text-2xl  font-bold">Social</h3>
 
-                <ul
-                  className="flex"
-                  style={{
-                    // border: "2px solid red",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    marginLeft: "0",
-                  }}
-                >
-                  {contactLinks.map((el) => (
-                    <a
-                      style={{ margin: "10px" }}
-                      href={el.link}
-                      className="md:ml-6 md:mr-0 mr-6 cursor-pointer mt-4 hover:scale-125 flex flex-col justify-center items-center"
-                    >
-                      <img alt="" src={el.url} />
-                    </a>
-                  ))}
-                </ul>
-              </div>
+              <ul className="flex">
+                {contactLinks.map((el) => (
+                  <a href={el.link}
+                    className="md:ml-6 md:mr-0 mr-6 cursor-pointer mt-4 hover:scale-125 flex flex-col justify-center items-center">
+                    <img alt="" src={el.url} />
+                  </a>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
@@ -135,81 +192,3 @@ export default Contact;
 //             like to say hello, send me a message. I'd love to hear from you.
 
 // afer get in Touch
-        // <div className="w-full md:pr-8">
-        //   <form
-        //   //  onSubmit={onSubmitHandler}
-        //   >
-        //     <div class="my-6">
-        //       <label
-        //         for="name"
-        //         class={
-        //           darkMode
-        //             ? "block mb-2 text-lg font-medium text-gray-900"
-        //             : "block mb-2 text-lg font-medium text-white"
-        //         }
-        //       >
-        //         Name
-        //       </label>
-        //       <input
-        //         type="text"
-        //         id="name"
-        //         name="name"
-        //         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        //         placeholder="Enter your name"
-        //         // onChange={onChangeHandler}
-        //         required
-        //       />
-        //     </div>
-        //     <div className="mb-4">
-        //       <label
-        //         for="email"
-        //         class={
-        //           darkMode
-        //             ? "block mb-2 text-lg font-medium text-gray-900"
-        //             : "block mb-2 text-lg font-medium text-white"
-        //         }
-        //       >
-        //         Email
-        //       </label>
-        //       <input
-        //         type="email"
-        //         id="email"
-        //         name="email"
-        //         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        //         placeholder="Enter your email"
-        //         // onChange={onChangeHandler}
-        //         required
-        //       />
-        //     </div>
-        //     <div className="mb-4">
-        //       <label
-        //         for="message"
-        //         class={
-        //           darkMode
-        //             ? "block mb-2 text-lg font-medium text-gray-900"
-        //             : "block mb-2 text-lg font-medium text-white"
-        //         }
-        //       >
-        //         Message
-        //       </label>
-        //       <textarea
-        //         id="message"
-        //         name="message"
-        //         class="bg-gray-50 border border-gray-300 text-gray-900 h-28 w-full text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        //         placeholder="Enter your message"
-        //         // onChange={onChangeHandler}
-        //         required
-        //       />
-        //     </div>
-        //     <div className="flex justify-between ">
-        //       <div className="underline">
-        //         <a href="mailto:rspatil3562@gmail.com">
-        //           Send me email directly
-        //         </a>
-        //       </div>
-        //       <button className="bg-indigo-500 text-white px-4 py-2 w-40 rounded-md hover:bg-indigo-400">
-        //         <a href="mailto:rspatil3562@gmail.com">Submit</a>
-        //       </button>
-        //     </div>
-        //   </form>
-        // </div>;
